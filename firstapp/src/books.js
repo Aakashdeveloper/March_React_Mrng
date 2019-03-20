2700d57a9cc51991ae9af0a0981230bfe557427b
aakashDeveloper <ahanda205@gmail.com>
2019-03-20 06:59:51 +0530

    a

---
+++ firstapp/src/books.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Books = () => {
+    return(
+        <div>
+            <hr/>
+             <h1> Books pAge</h1>
+        </div>
+    )
+}
+
+export default Books;
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Books from './books';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Home = () => {
+    return(
+        <div>
+            <h1>My First Page of React</h1>
+            <h2>This is second tag</h2>
+            <Books></Books>
+        </div>
+    )
+}
+
+ReactDOM.render(<Home/>,document.getElementById('root'))
